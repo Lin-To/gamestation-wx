ca6cec3ebfd24c65490f0aaa93a76af5ba75a47e
Lin To <linxuanto@gmail.com>
2019-08-01 20:23:50 +0800

    template for wish list page

---
+++ pages/wishList/wishList.js
@@ -0,0 +1,76 @@
+// pages/wishList/wishList.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    let page = this
+    // let name = app.globalData.name
+
+    wx.request({
+      // url: 'http://localhost:3000/api/v1/games',
+      url: 'https://gamestation.herokuapp.com/api/v1/games',
+      success: function (res) {
+        console.log(res)
+        page.setData({ games: res.data.games })
+      }
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
