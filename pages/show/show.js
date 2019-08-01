9270479ed9c2b85815bca31021ae40bd73b2e00c
Lin To <linxuanto@gmail.com>
2019-08-01 15:49:47 +0800

    showpage time picker with style

--- pages/show/show.js
+++ pages/show/show.js
@@ -7,6 +7,8 @@ Page({
    * Page initial data
    */
   data: {
+    currentDate: new Date().getTime(),
+    minDate: new Date().getTime(),
     imgUrls: ["https://images.unsplash.com/photo-1559984859-d568d6eeba0f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"],
     loading: false,
   },
